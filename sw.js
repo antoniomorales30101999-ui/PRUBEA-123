ffd13f5c18bec393982098c02e85bf504f3dcba6
PACTOV13 <antonio.morales.30101999@gmail.com>
2026-08-19 09:20:16 -0600

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,28 +1,18 @@
-const CACHE_NAME = 'tecamac-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-  'https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css',
-  'https://code.jquery.com/jquery-3.7.0.min.js',
-  'https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js',
-  'https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js'
-];
+const CACHE_NAME = 'tecamac-v2';
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
+  // Intenta obtener la información de la red; si falla o no hay conexión, busca en el caché
   e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
     })
   );
 });
